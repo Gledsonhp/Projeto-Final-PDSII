4f80a6f43e50d688c4c6bfab3d83f5293042fa6d
Gledson Paiva <gleds.hpaiva2@gmail.com>
2025-01-29 22:18:04 -0300

    Update othello.hpp

--- src/games/othello.hpp
+++ src/games/othello.hpp
@@ -1,32 +1,34 @@
 #ifndef OTHELLO_HPP
 #define OTHELLO_HPP
 
-#include <vector>
 #include <memory>
-#include "jogador.hpp"
+#include <vector>
+#include <iostream>
+#include "Jogador.hpp"
 
 class Othello {
 public:
     Othello(std::shared_ptr<Jogador> j1, std::shared_ptr<Jogador> j2);
-
     void jogar();
+    void exibirTabuleiro();
+    void exibirJogadorAtual();
+    void exibirVencedor();
 
 private:
     void inicializarTabuleiro();
     bool jogadaValida(int linha, int coluna);
     bool verificarCaptura(int linha, int coluna, int dL, int dC, char jogador);
     void capturarPecas(int linha, int coluna, char jogador);
+    void transformarPecas(int linha, int coluna, int dL, int dC, char jogador);
     bool verificarFimDeJogo();
-    void exibirJogadorAtual();
-    void exibirTabuleiro();
-    void transformarPecas(int linha, int coluna, char jogador);
     int contarPecas(char jogador);
-    void exibirVencedor();
 
-    std::shared_ptr<Jogador> jogador1;
-    std::shared_ptr<Jogador> jogador2;
-    bool turnoJogador1;
     std::vector<std::vector<char>> tabuleiro;
+    std::shared_ptr<Jogador> jogador1, jogador2;
+    bool turnoJogador1;
+
+    std::vector<std::pair<int, int>> pecasCapturadasJogador1;
+    std::vector<std::pair<int, int>> pecasCapturadasJogador2;
 };
 
 #endif // OTHELLO_HPP
