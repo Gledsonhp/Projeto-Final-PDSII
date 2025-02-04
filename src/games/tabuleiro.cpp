506b8e4942e3aa9e8f9c0c9bf0318575b3aeab1c
Gledson Paiva <gleds.hpaiva2@gmail.com>
2025-02-03 21:12:03 -0300

    Update tabuleiro.cpp

--- src/games/tabuleiro.cpp
+++ src/games/tabuleiro.cpp
@@ -1,6 +1,7 @@
 #include "tabuleiro.hpp"
 
-Tabuleiro::Tabuleiro() : linhas(0), colunas(0), tabuleiro(0, std::vector<char>(0)) {
+Tabuleiro::Tabuleiro() : linhas(0), colunas(0) {
+
 }
 
 Tabuleiro::Tabuleiro(int tamanho) : linhas(tamanho), colunas(tamanho) {
@@ -8,7 +9,7 @@ Tabuleiro::Tabuleiro(int tamanho) : linhas(tamanho), colunas(tamanho) {
 }
 
 Tabuleiro::Tabuleiro(int linhas, int colunas) : linhas(linhas), colunas(colunas) {
-    tabuleiro.resize(linhas, std::vector<char>(colunas, ' '));
+    tabuleiro.resize(linhas, std::vector<char>(colunas, ' ')); 
 }
 
 void Tabuleiro::exibirTabuleiro() const {
@@ -18,19 +19,19 @@ void Tabuleiro::exibirTabuleiro() const {
         }
         std::cout << std::endl;
         if (i < linhas - 1) {
-            std::cout << std::string(colunas * 2 - 1, '-') << std::endl;
+            std::cout << std::string(colunas * 2 - 1, '-') << std::endl; 
         }
     }
 }
 
 bool Tabuleiro::estaVazio(int linha, int coluna) const {
-    return tabuleiro[linha][coluna] == ' ';
+    return tabuleiro[linha][coluna] == ' '; 
 }
 
 void Tabuleiro::setPosicao(int linha, int coluna, char valor) {
-    tabuleiro[linha][coluna] = valor;
+    tabuleiro[linha][coluna] = valor;  
 }
 
 char Tabuleiro::getPosicao(int linha, int coluna) const {
-    return tabuleiro[linha][coluna];
+    return tabuleiro[linha][coluna]; 
 }
